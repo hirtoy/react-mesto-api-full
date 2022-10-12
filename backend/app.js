e891d181615c9ddad54d46fbeaed20ae5237b7af
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-13 01:04:59 +0300

    правки

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,12 @@ app.use(cors({
 
 app.use(cookieParser());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
