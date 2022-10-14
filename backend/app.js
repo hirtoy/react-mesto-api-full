255397f27e80a3626b024e6da4054253fdff0d51
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-14 18:46:55 +0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
@@ -16,15 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      'https://hirtoy.nomoredomains.icu',
-      'http://hirtoy.nomoredomains.icu',
-    ],
-    credentials: true,
-  }),
-);
+app.use(require('./middelewares/cors'));
 
 app.use(cookieParser());
 
---
+++ backend/middelewares/cors.js
@@ -0,0 +1,30 @@
+const allowedCors = [
+  'https://hirtoy.nomoredomains.icu',
+  'http://hirtoy.nomoredomains.icu',
+  'http://api.hirtoy.nomoredomains.icu',
+  'https://api.hirtoy.nomoredomains.icu',
+];
+
+// eslint-disable-next-line consistent-return
+module.exports = ((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('X-Server', 'test');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
