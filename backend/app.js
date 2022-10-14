411a8b80b0070c981ba43a73e6d656578435ed2d
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-14 18:35:47 +0300

    fixed cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
+const cors = require('./middelewares/cors');
 const routerUser = require('./routes/users');
 const routerCards = require('./routes/cards');
 const { login, createUsers } = require('./controllers/users');
