c5284782b0cdddab1b786ba66cb0910dc3861dd7
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-15 23:26:29 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+
 const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/unauthorized-error');
